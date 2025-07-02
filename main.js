76aa0a1613e6862fdc2b66377ab26b420c2e190d
loremercadologa <140033755+Loremercadologa@users.noreply.github.com>
2025-07-01 22:47:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Brindo asesoría y desarrollo proyectos de mercadotecnia y negocios para emprendedores y empresas.')/**Poner a que me dedico poner algo corto */
+  .typeString('Brindo asesoría a negocios  y desarrollo proyectos de mercadotecnia para emprendedores y empresas.')/**Poner a que me dedico poner algo corto */
   .pauseFor(200)
   .deleteChars(10)
   .start();
